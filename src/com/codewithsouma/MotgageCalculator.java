2507051487684269e69b4306868f0ffb0227be85
Souma <souma.hitech@gmail.com>
2020-11-11 21:02:27 +0530

    add payment schedule

--- src/com/codewithsouma/MotgageCalculator.java
+++ src/com/codewithsouma/MotgageCalculator.java
@@ -1,6 +1,7 @@
 package com.codewithsouma;
 
 import java.text.NumberFormat;
+import java.util.Currency;
 import java.util.Scanner;
 
 public class MotgageCalculator {
@@ -11,7 +12,32 @@ public class MotgageCalculator {
 
         //Calculate mortgage
         double mortgage = calculateMortgage(principal, annualInterestRate, years);
-        System.out.print("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+        System.out.println("\nMORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + NumberFormat.getCurrencyInstance().format(mortgage));
+
+        //displaying payment schedule
+        displayPaymentSchedule(principal, annualInterestRate, years);
+    }
+
+    public static void displayPaymentSchedule(int principal, float annualInterestRate, byte years) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+        float monthlyInterestRate = annualInterestRate / (MONTHS_IN_YEAR * PERCENT);
+        double balance = 0;
+
+        System.out.println("\nPAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short paymentNo = 1; paymentNo <= numberOfPayments; paymentNo++) {
+            balance = (principal * (Math.pow((1 + monthlyInterestRate), numberOfPayments)
+                    - Math.pow((1 + monthlyInterestRate), paymentNo))) /
+                    (Math.pow((1 + monthlyInterestRate), numberOfPayments) - 1);
+
+            String formattedBalance = NumberFormat.getCurrencyInstance().format(balance);
+            System.out.println(formattedBalance);
+        }
+
     }
 
     public static double readNumber(String prompt, double min, double max) {
