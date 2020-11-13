8c4a071283717229d847015ca710d285a494ddea
Souma <souma.hitech@gmail.com>
2020-11-13 21:29:17 +0530

    Extract numberFormat instance and create a field in MortgageReport class

--- src/com/codewithsouma/MortgageReport.java
+++ src/com/codewithsouma/MortgageReport.java
@@ -4,10 +4,12 @@ import java.text.NumberFormat;
 
 public class MortgageReport {
 
+    private final NumberFormat currency;
     private MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
 
     public void printPaymentSchedule() {
@@ -15,12 +17,12 @@ public class MortgageReport {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
         for (double balance : calculator.getRemainingBalances())
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+            System.out.println(currency.format(balance));
     }
 
     public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
-        String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
+        String formattedMortgage = currency.format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
